d9184ec89d9f5727f800dde35dc297be6f81010f
pratikk3008 <148140944+pratikk3008@users.noreply.github.com>
2024-11-11 21:37:33 +0530

    Update A3.py

--- A3.py
+++ A3.py
@@ -1,55 +1,40 @@
-import heapq 
+# Python3 program to demonstrate working of Alpha-Beta Pruning
+# Initial values of Alpha and Beta
+MAX, MIN = 1000, -1000
+# Returns optimal value for current player (Initially called for root and maximizer)
+def minimax(depth, nodeIndex, maximizingPlayer, values, alpha, beta):
 
-class node: 
-    def __init__(self, freq, symbol, left=None, right=None): 
-        self.freq = freq # frequency of symbol 
-        self.symbol = symbol # symbol name (character) 
-        self.left = left # node left of current node 
-        self.right = right # node right of current node 
-        self.huff = '' # tree direction (0/1) 
-    def __lt__(self, nxt): 
-        return self.freq < nxt.freq 
-  
-# utility function to print huffman codes for all symbols in the newly created Huffman tree 
-def printNodes(node, val=''): 
-  
-    newVal = val + str(node.huff)     # huffman code for current node 
-  
-    # if node is not an edge node then traverse inside it 
-    if(node.left): 
-        printNodes(node.left, newVal) 
-    if(node.right): 
-        printNodes(node.right, newVal) 
-  
-        # if node is edge node then display its huffman code 
-    if(not node.left and not node.right): 
-       print(node.symbol + " -> " + newVal) 
+    # Terminating condition: leaf node is reached
+    if depth == 3:
+        return values[nodeIndex]
 
-chars = ['a', 'b', 'c', 'd', 'e', 'f'] # characters for huffman tree 
-freq = [5, 9, 12, 13, 16, 45] # frequency of characters
-   
-nodes = [] # list containing unused nodes 
+    if maximizingPlayer:
+        best = MIN
 
-# converting characters and frequencies into huffman tree nodes 
-for x in range(len(chars)): 
-    heapq.heappush(nodes, node(freq[x], chars[x])) 
-print(nodes)
-print(" ")
-print(" ") 
-while len(nodes) > 1: 
-  
-    # sort all the nodes in ascending order based on their frequency 
-    left = heapq.heappop(nodes) 
-    right = heapq.heappop(nodes) 
-  
-    # assign directional value to these nodes 
-    left.huff = 0
-    right.huff = 1
-  
-    # combine the 2 smallest nodes to create new node as their parent 
-    newNode = node(left.freq+right.freq, left.symbol+right.symbol, left, right) 
-  
-    heapq.heappush(nodes, newNode) 
-  
-# Huffman Tree is ready! 
-printNodes(nodes[0])
+        # Recur for left and right children
+        for i in range(0, 2):
+            val = minimax(depth + 1, nodeIndex * 2 + i, False, values, alpha, beta)
+            best = max(best, val)
+            alpha = max(alpha, best)
+
+            # Alpha Beta Pruning
+            if beta <= alpha:
+                break
+        return best
+    
+    else:
+        best = MAX
+
+        # Recur for left and right children
+        for i in range(0, 2):
+            val = minimax(depth + 1, nodeIndex * 2 + i, True, values, alpha, beta)
+            best = min(best, val)
+            beta = min(beta, best)
+
+            # Alpha Beta Pruning
+            if beta <= alpha:
+                break
+        return best
+
+values = [3, 5, 6, 9, 1, 2, 0, -1]
+print("The optimal value is :", minimax(0, 0, True, values, MIN, MAX))
